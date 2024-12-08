967fe422f914da4062d79eed4774ad72611d0208
dombinik <dombitv69@gmail.com>
2024-12-08 21:31:36 +0800

    database

--- DATABASE FILE/realestatephp.sql
+++ DATABASE FILE/realestatephp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2024 at 01:31 PM
+-- Generation Time: Dec 08, 2024 at 02:31 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
